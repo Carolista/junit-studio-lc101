446b14a8d017f48e3cb7641384ed37bca187b2a1
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-26 19:34:49 -0600

    added TODOs for bonus mission

---
+++ src/test/BonusBinarySearchTest.java
@@ -0,0 +1,49 @@
+package test;
+
+// TODO: import JUnit, assertion methods
+
+
+// TODO: import BonusBinarySearch class from main package
+
+
+public class BonusBinarySearchTest {
+
+    // TODO: Initialize reusable arrays of sorted integers to use as test data
+
+
+    // POSITIVE TEST CASES
+
+    // TODO: Test a number being found somewhere in the first half of the array
+
+
+    // TODO: Test a number being found somewhere in the second half of the array
+
+
+    // TODO: Test a number being found exactly in the middle of an array with an odd length
+
+
+    // TODO: Test a number being found at the very beginning of an array
+
+
+    // TODO: Test a number being found at the very end of an array
+
+
+    // TODO: Test an array that includes negative numbers
+
+
+    // NEGATIVE TEST CASES
+
+    // TODO: Test a number not being found in an array
+
+
+    // EDGE CASES
+
+    // TODO: Test finding a number in an array of length 1
+
+
+    // TODO: Test an empty array
+
+
+}
+
+
