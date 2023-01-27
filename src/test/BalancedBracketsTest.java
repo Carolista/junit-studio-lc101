4c5145923f7227277bf266b114e184eb9e75be81
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-26 19:28:59 -0600

    added TODOs to master

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -4,13 +4,61 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import main.BalancedBrackets;
+
+// TODO: import BalancedBrackets class from main package
+
+
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
-    @Test
-    public void emptyTest() {
-        assertEquals(true, true);
-    }
+    // POSITIVE TEST CASES
+
+    // TODO: Refactor example test from instructions to use AAA pattern and message parameter
+
+
+    // TODO: Test balanced brackets in front of other characters — "[]LaunchCode"
+
+
+    // TODO: Test balanced brackets around other characters — "[LaunchCode]"
+
+
+    // TODO: Test balanced brackets within other characters — "Launch[Code]"
+
+
+    // TODO: Test multiple consecutive pairs of balanced brackets — "[][][]"
+
+
+    // TODO: Test multiple nested balanced brackets — "[[[]]]"
+
+
+
+    // NEGATIVE TEST CASES
+
+    // TODO: Test single opening bracket — "["
+
+
+    // TODO: Test single closing bracket — "]"
+
+
+    // TODO: Test reversed brackets — "]["
+
+
+    // TODO: Test single bracket with other characters "[LaunchCode"
+
+
+    // TODO: Test unbalanced brackets among other characters — "Launch]Code["
+
+
+    // TODO: Test a series of mixed, unbalanced brackets — "[[]][]["
+
+
+
+    // EDGE CASES
+
+    // TODO: Test string with only characters other than brackets — "LaunchCode"
+
+
+    // TODO: Test empty string — ""
 
 
 }
